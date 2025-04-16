13914d91e9be8df187cbda6a3c7599f49a731de3
xushunbin <xushunbin1990@gmail.com>
2025-04-16 14:34:03 +0800

    增加售后返回数据结构

--- src/requests/OarqRequest.php
+++ src/requests/OarqRequest.php
@@ -3,12 +3,14 @@
 namespace ds\requests;
 
 use ds\BaseRequest;
+use ds\responses\OarqResponse;
 
 /**
  * 接口名称简写
  */
 class OarqRequest extends BaseRequest
 {
+    public ?string $responseClass = OarqResponse::class;
     /**
      * 店铺售后查询
      * @see http://wiki.yitaosoft.com/#/share/Rd8xagXg/jbXKOEX7
