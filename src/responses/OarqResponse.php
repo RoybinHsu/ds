2bb7f53c5db0f6eea71d1dff3fc7b83708ec8463
xushunbin <xushunbin1990@gmail.com>
2025-04-17 10:41:04 +0800

    OarqContent标注方法

--- src/responses/OarqResponse.php
+++ src/responses/OarqResponse.php
@@ -3,6 +3,7 @@
 namespace ds\responses;
 
 use ds\BaseModel;
+use ds\models\OarqContent;
 use ds\models\OarqModel;
 use ds\models\OrderContentModel;
 
@@ -10,9 +11,9 @@ class OarqResponse extends BaseModel
 {
     /**
      * 售后结果
-     * @var OrderContentModel|null
+     * @var OarqContent|null
      */
-    public ?OrderContentModel $content = null;
+    public ?OarqContent $content = null;
 
     /**
      * 是否有下一页； true:有，false:无
