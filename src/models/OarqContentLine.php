ffb84445939432a121251bf6156d37e0213b0aad
xushunbin <xushunbin1990@gmail.com>
2025-04-22 11:45:08 +0800

    OarqContentLine增加type

--- src/models/OarqContentLine.php
+++ src/models/OarqContentLine.php
@@ -4,6 +4,41 @@ namespace ds\models;
 
 use ds\BaseModel;
 
+/**
+ * Setter 方法组
+ * * @method self setRefAid(?string $refAid) 设置平台售后单ID
+ * * @method self setRefAlId(?string $refAlId) 设置平台售后子单ID
+ * * @method self setRefOid(?string $refOid) 设置平台订单ID
+ * * @method self setRefOlId(?string $refOlId) 设置平台子订单ID
+ * * @method self setTotalFee(?float $totalFee) 设置总收入
+ * * @method self setRefundFee(?float $refundFee) 设置退款金额
+ * * @method self setPrice(?float $price) 设置商品单价
+ * * @method self setNum(?int $num) 设置商品数量
+ * * @method self setRefSpuId(?string $refSpuId) 设置平台SPU_ID
+ * * @method self setRefSkuId(?string $refSkuId) 设置平台SKU_ID
+ * * @method self setOuterId(?string $outerId) 设置商家编码
+ * * @method self setTitle(?string $title) 设置商品标题
+ * * @method self setRefBoughtSkuId(?string $refBoughtSkuId) 设置购买SKU（换出SKU）
+ * * @method self setRefundStatus(?string $refundStatus) 设置退款状态
+ * * @method self setType(?int $type) 设置类型标识
+ * *
+ * * // Getter 方法组
+ * * @method string|null getRefAid() 获取平台售后单ID
+ * * @method string|null getRefAlId() 获取平台售后子单ID
+ * * @method string|null getRefOid() 获取平台订单ID
+ * * @method string|null getRefOlId() 获取平台子订单ID
+ * * @method float|null getTotalFee() 获取总收入
+ * * @method float|null getRefundFee() 获取退款金额
+ * * @method float|null getPrice() 获取商品单价
+ * * @method int|null getNum() 获取商品数量
+ * * @method string|null getRefSpuId() 获取平台SPU_ID
+ * * @method string|null getRefSkuId() 获取平台SKU_ID
+ * * @method string|null getOuterId() 获取商家编码
+ * * @method string|null getTitle() 获取商品标题
+ * * @method string|null getRefBoughtSkuId() 获取购买SKU（换出SKU）
+ * * @method string|null getRefundStatus() 获取退款状态
+ * * @method int|null getType() 获取类型标识
+ */
 class OarqContentLine extends BaseModel
 {
     /**
@@ -89,4 +124,10 @@ class OarqContentLine extends BaseModel
      * @var string|null
      */
     public ?string $refundStatus = null;
+
+    /**
+     * 类型
+     * @var int|null
+     */
+    public ?int $type = null;
 }
