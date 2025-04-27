5d315d9ff7eb543926e4b0740598507d745041da
xushunbin <xushunbin1990@gmail.com>
2025-04-27 11:44:13 +0800

    增加获取 平台店铺商品SKU同步(按时间)

--- src/requests/PssbRequest.php
+++ src/requests/PssbRequest.php
@@ -4,9 +4,13 @@ namespace ds\requests;
 
 use ds\BaseRequest;
 
+/**
+ * @see http://wiki.yitaosoft.com/#/share/5wXnJGX9/57zL3aXQ
+ */
 class PssbRequest extends BaseRequest
 {
 
+    public ?string $responseClass = null;
     /**
      * @inheritDoc
      */
